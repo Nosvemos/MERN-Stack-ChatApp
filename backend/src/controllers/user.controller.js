443f4c41a2b68057547313819e9552a49fe1e66e
Nosvemos <sametozturk1881@gmail.com>
2025-02-25 18:18:47 +0300

    Console.error adjustments

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -7,7 +7,7 @@ export const updateProfile = async (req, res) => {
 		const userId = req.user._id;
 
 		if(!profilePic) {
-			return res.status(400).json({error: 'Profile picture not found'});
+			return res.status(400).json({error: 'Profile picture not found.'});
 		}
 
 		const uploadResponse = await cloudinary.uploader.upload(profilePic);
@@ -20,7 +20,7 @@ export const updateProfile = async (req, res) => {
 		res.status(200).json(updatedUser);
 
 	} catch (error) {
-		console.log('Error in updating profile', error);
+		console.error('Error in updating profile:', error.message);
 		res.status(500).json({message: 'Internal Server Error'});
 	}
 };
@@ -29,7 +29,7 @@ export const checkAuth = (req, res) => {
 	try {
 		res.status(200).json(req.user);
 	} catch (error) {
-		console.log('Error in checkAuth controller', error.message);
+		console.error('Error in checkAuth controller:', error.message);
 		res.status(500).json({message: 'Internal Server Error'});
 	}
 };
