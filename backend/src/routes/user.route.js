17ed957016be00fce29f70d72430ff41fd967dbb
Nosvemos <sametozturk1881@gmail.com>
2025-02-25 17:59:07 +0300

    protectRoute middleware

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import { updateProfile } from '../controllers/user.controller.js';
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post('/update-profile', updateProfile);
+router.post('/update-profile', protectRoute, updateProfile);
 
 export default router;
