1d3324dace5cce338ce478ef3aa365c61687f66a
Nosvemos <sametozturk1881@gmail.com>
2025-02-25 17:30:21 +0300

    Login controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -46,10 +46,37 @@ export const signup = async (req, res) => {
 	}
 };
 
-export const login = (req, res) => {
-	res.send('Login route');
+export const login = async (req, res) => {
+	const { email, password } = req.body;
+	try {
+		if(!email || !password) {
+			return res.status(400).json({ message: 'All fields are required.' });
+		}
+
+		const user = await User.find({email});
+		if (!user) {
+			return res.status(400).json({ message: 'Invalid credentials.' });
+		}
+
+		const isPasswordCorrect = await bcrypt.compare(password, user.password);
+		if(!isPasswordCorrect) {
+			return res.status(400).json({ message: 'Invalid credentials.' }); // Not showing customized message due to security reasons.
+		}
+
+		generateToken(user._id, res);
+
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			email: user.email,
+			profilePic: user.profilePic
+		});
+	} catch (error) {
+		console.log('Error in login controller', error.message);
+		res.status(500).json({ message: 'Internal Server Error' });
+	}
 };
 
 export const logout = (req, res) => {
-	res.send('Logout route');
+
 };
