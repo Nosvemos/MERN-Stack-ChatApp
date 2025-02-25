b1dcfb8805e02bd8e43969fa7fbaac8b9c010d09
Nosvemos <sametozturk1881@gmail.com>
2025-02-25 18:12:40 +0300

    updateProfile controller

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,26 @@
 import User from '../models/user.model.js';
+import cloudinary from "../lib/cloudinary.js";
 
 export const updateProfile = async (req, res) => {
+	try {
+		const { profilePic } = req.body;
+		const userId = req.user._id;
 
+		if(!profilePic) {
+			return res.status(400).json({error: 'Profile picture not found'});
+		}
+
+		const uploadResponse = await cloudinary.uploader.upload(profilePic);
+		if(!uploadResponse) {
+			return res.status(500).json({error: 'Profile picture service is not giving response.'});
+		}
+
+		const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+		res.status(200).json(updatedUser);
+
+	} catch (error) {
+		console.log('Error in updating profile', error);
+		res.status(500).json({message: 'Internal Server Error'});
+	}
 };
