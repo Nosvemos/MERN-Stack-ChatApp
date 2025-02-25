e64e2cf75a08df96d33d8fbf6b7fbb51a5e57594
Nosvemos <sametozturk1881@gmail.com>
2025-02-25 20:39:16 +0300

    CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import {connectDB} from "./lib/db.js";
 
@@ -15,6 +16,13 @@ const port = process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors(
+	{
+		origin: 'http://localhost:5173', // Vite port
+		credentials: true,
+
+	}
+));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/user', userRoutes);
