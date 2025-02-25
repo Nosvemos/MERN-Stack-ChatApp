f41e74649792f1dfbd2aa7e5aa712db9e5274bfd
Nosvemos <sametozturk1881@gmail.com>
2025-02-25 18:15:28 +0300

    checkAuth controller

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 
-import { updateProfile } from '../controllers/user.controller.js';
+import { updateProfile, checkAuth } from '../controllers/user.controller.js';
 import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.post('/update-profile', protectRoute, updateProfile);
+
+router.get('/check', protectRoute, checkAuth);
 
 export default router;
