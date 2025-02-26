a32d7044ab591da84741eda420f83b39f65086a8
Nosvemos <sametozturk1881@gmail.com>
2025-02-26 13:17:39 +0300

    Sidebar created basically

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,4 +1,17 @@
+import {useChatStore} from "../store/useChatStore.js";
+import {useEffect} from "react";
+
 const Sidebar = () => {
+	const { getUsers, users, selectedUser, setSelectedUser, isUsersLoading } = useChatStore();
+	const onlineUsers = [];
+
+	useEffect(() => {
+		getUsers();
+	}, [getUsers])
+
+	if(isUsersLoading) {
+		
+	}
 	return (
 		<div>Sidebar</div>
 	);
