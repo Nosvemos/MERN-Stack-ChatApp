c06cfb24ee85be0d3049482b92d12bd2155c7e29
Nosvemos <sametozturk1881@gmail.com>
2025-02-26 13:35:57 +0300

    Update ProfilePage.jsx

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -35,7 +35,7 @@ const ProfilePage = () => {
 	return (
 		<div className='h-screen pt-20'>
 			<div className='max-w-2xl mx-auto p-4 py-8'>
-				<div className='bg-base-200 rounded-xl p-6 space-y-8 border'>
+				<div className='bg-base-300/20 rounded-xl p-6 space-y-8 shadow-xl'>
 					<div className="text-center">
 						<h1 className="text-2xl font-semibold ">Profile</h1>
 						<p className="mt-2">Your profile information</p>
