6afcd9294ebd8262b84b7ac6a7c57c19956762a2
Nosvemos <sametozturk1881@gmail.com>
2025-02-26 16:18:32 +0300

    receiving and sending messages via socketio

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -11,9 +11,11 @@ const io = new Server(server, {
 	}
 });
 
-const userSocketMap = {
+export function getReceiverSocketId(userId) {
+	return userSocketMap[userId];
+}
 
-};
+const userSocketMap = {};
 
 io.on('connection', (socket) => {
 	console.log(`A user connected: ${socket.id}`);
--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -11,13 +11,15 @@ import MessageSkeleton from "./skeletons/MessageSkeleton.jsx";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
-	const { messages, getMessages, isMessagesLoading, selectedUser } = useChatStore();
+	const { messages, getMessages, isMessagesLoading, selectedUser, subscribeToMessages, unsubscribeFromMessages } = useChatStore();
 	const { authUser } = useAuthStore();
 	const messageEndRef = useRef(null);
 
 	useEffect(() => {
 		getMessages(selectedUser._id);
-	}, [selectedUser._id, getMessages]);
+		subscribeToMessages();
+		return () => unsubscribeFromMessages();
+	}, [selectedUser._id, getMessages, subscribeToMessages, unsubscribeFromMessages]);
 
 	useEffect(() => {
 		if (messageEndRef.current && messages) {
